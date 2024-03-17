a18857cccc98f6e1f1974fd477f6107536ef4635
ciro9527 <ciro9527@gmail.com>
2024-03-17 20:52:49 +0800

    update lint

--- lua/options.lua
+++ lua/options.lua
@@ -87,6 +87,15 @@ lspconfig.rubocop.setup({
 })
 
 
+vim.diagnostic.config({ virtual_text = false, underline = false, signs=false })
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
